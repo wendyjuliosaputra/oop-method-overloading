faf4a5c85c8fcd9633390eb2869b12a30bce2f48
wendyjuliosaputra <124264447+wendyjuliosaputra@users.noreply.github.com>
2023-03-15 13:07:01 +0700

    Add files via upload

---
+++ v28.cs
@@ -0,0 +1,12 @@
+namespace v28
+{
+    class Rumus
+    {
+        public int persegi (int s){
+            return s * s;           
+        }
+        public int persegi(int p, int l){
+            return p * l;
+        }
+    }
+}
